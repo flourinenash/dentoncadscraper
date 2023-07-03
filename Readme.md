25befee77d6c3468966df4613d5261a1bf3242f5
flourinenash <flourinenash@gmail.com>
2023-07-03 14:19:37 -0500

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,32 @@
+Property Data Scraper
+This Python script scrapes property data from the True Automation Property Search website for a range of property IDs and years, and saves the data to a CSV file.
+
+Requirements
+To run this script, you need:
+
+Python 3.x
+The requests library
+The beautifulsoup4 library
+A range of property IDs to search
+Usage
+Clone this repository to your local machine.
+Install the required libraries using pip install -r requirements.txt.
+Open scraper.py in a text editor.
+Modify the base_url variable to match the URL of the property search page for your county or city.
+Modify the prop_id range in the for loop to match the range of property IDs you want to search.
+Run the script using python scraper.py.
+The script will create a CSV file named property_data.csv in the same directory as the script, and write the scraped data to the file.
+Output
+The CSV file contains the following columns:
+
+Property ID: The ID of the property searched.
+Year: The year of the property data searched.
+Address: The address of the property searched.
+Market Value: The market value of the property searched for the given year.
+If the market value is not found for a given year, the value in the Market Value column will be "Not found".
+
+Limitations
+This script assumes that the property search page has the same structure, and that the property data is formatted in the same way. If the structure or formatting of the page changes, this script may need to be updated accordingly.
+
+License
+This script is released under the MIT License.
---
+++ Scraper.py
@@ -0,0 +1,45 @@
+import csv
+import re
+import requests
+from bs4 import BeautifulSoup
+
+# Define the base URL for the property search
+base_url = 'https://propaccess.trueautomation.com/clientdb/Property.aspx?cid=19&prop_id={}&year={}'
+
+# Open a CSV file for writing and write the header row
+with open('property_data.csv', 'w', newline='') as csvfile:
+    fieldnames = ['Property ID', 'Year', 'Address', 'Market Value']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    # Loop over the property IDs to search
+    for prop_id in range(180440, 180460):
+        # Send a GET request to the property search URL for the latest year (2023)
+        response = requests.get(base_url.format(prop_id, 2023))
+        soup = BeautifulSoup(response.content, 'html.parser')
+        text = soup.get_text()
+
+        # Extract the property address from the text using a regular expression
+        address_match = re.search(r'Address:(.*?)Mapsco:', text, re.DOTALL)
+        if address_match:
+            address = address_match.group(1).strip()
+
+        # Loop over the years to search for property data
+        for year in range(2010, 2024):
+            # Format the property search URL with the current property ID and year
+            url = base_url.format(prop_id, year)
+
+            # Send a GET request to the property search URL
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            text = soup.get_text()
+
+            # Extract the property market value from the text using a regular expression
+            market_value_match = re.search(r'Market Value:=\s*\$?([\d,]+)', text)
+            if market_value_match:
+                market_value = market_value_match.group(1).replace(',', '')
+                # Write a row to the CSV file with the property data
+                writer.writerow({'Property ID': prop_id, 'Year': year, 'Address': address, 'Market Value': market_value})
+            else:
+                # Write a row to the CSV file with a "Not found" market value
+                writer.writerow({'Property ID': prop_id, 'Year': year, 'Address': address, 'Market Value': 'Not found'})
